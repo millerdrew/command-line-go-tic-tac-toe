540eae1ec62d139c9a47148cc36251d5fb653310
Drew Miller <drew.miller@limelighthealth.com>
2019-11-09 20:39:24 -0500

    Add input validation

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 // Two-player Go Command Line Tic Tac Toe
 /*
   TODO:
-  Check for win/lose condition
   Computerized AI
 */
 
@@ -9,8 +8,40 @@ package main
 
 import "fmt"
 import "strconv"
+import "regexp"
 
+var validNumber = regexp.MustCompile("^[1-9]{1}$")
 type Board [3][3]string
+
+func createBoard() Board {
+  x := 0
+  var b Board
+  for i := 0; i < 3; i++ {
+    for j := 0; j < 3; j++ {
+      x++
+      b[i][j] = strconv.Itoa(x)
+    }
+  }
+  return b
+}
+
+func printBoard(b Board) {
+  fmt.Printf("\t%s|%s|%s\n\t%s|%s|%s\n\t%s|%s|%s\n", b[0][0], b[0][1], b[0][2], b[1][0], b[1][1], b[1][2], b[2][0], b[2][1], b[2][2])
+}
+
+func markBoard(b Board, player int, input string) Board {
+  for i := 0; i < 3; i++ {
+    for j := 0; j < 3; j++ {
+      if b[i][j] == input {
+        switch player {
+          case 1: b[i][j] = "X"
+          case 2: b[i][j] = "O"
+        }
+      }
+    }
+  }
+  return b
+}
 
 func checkWinner(b Board, player int) bool {
   var mark string
@@ -43,51 +74,34 @@ func checkWinner(b Board, player int) bool {
   return false
 }
 
-func createBoard() Board {
-  x := 0
-  var b Board
-  for i := 0; i < 3; i++ {
-    for j := 0; j < 3; j++ {
-      x++
-      b[i][j] = strconv.Itoa(x)
-    }
+func getInput(b Board, player int) (string, bool) { 
+  fmt.Printf("Player %d: Make your move, select a # 1-9 (or 'exit')\n", player)
+  var input string
+  fmt.Scanf("%s", &input)
+  if !validNumber.MatchString(input) {
+    fmt.Println("Invalid input.  Please try again.")
+    return input, false
   }
-  return b
-}
-
-func printBoard(b Board) {
-  fmt.Printf("\t%s|%s|%s\n\t%s|%s|%s\n\t%s|%s|%s\n", b[0][0], b[0][1], b[0][2], b[1][0], b[1][1], b[1][2], b[2][0], b[2][1], b[2][2])
-}
-
-func markBoard(b Board, player int, input string) Board {
-  for i := 0; i < 3; i++ {
-    for j := 0; j < 3; j++ {
-      if b[i][j] == input {
-        switch player {
-          case 1: b[i][j] = "X"
-          case 2: b[i][j] = "O"
-        }
-      }
-    }
-  }
-  return b
+  return input, true
 }
 
 func mainLoop() {
-  gameOver := 0
+  gameOver := false 
   player := 1
   b := createBoard()
   moves := 0
-  for gameOver == 0 {
+  for gameOver == false {
     moves++
-    fmt.Printf("Player %d: Make your move, select a # 1-9 (or 'exit')\n", player)
+    validInput := false
     var input string
-    fmt.Scanf("%s", &input)
-    b = markBoard(b, player, input)
+    for validInput == false {
+      input, validInput = getInput(b, player)
+      b = markBoard(b, player, input)
+    }
     printBoard(b)
     if checkWinner(b, player) == true {
-      fmt.Printf("Player %d wins!", player)
-      gameOver = 1
+      fmt.Printf("Player %d wins!\n", player)
+      gameOver = true
       break
     }
     switch player {
@@ -96,11 +110,11 @@ func mainLoop() {
     }
     if input == "exit" {
       fmt.Println("Game over")
-      gameOver = 1
+      gameOver = true
     }
     if moves == 9 {
       fmt.Println("It's a tie!")
-      gameOver = 1
+      gameOver = true
     }
   }
 }
